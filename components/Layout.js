040b011142779807878703e4f47d86d190581e16
Nelson Estevão <nelsonmestevao@gmail.com>
2021-01-28 02:10:27 +0000

    Split social icons

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import SocialIcons from "./SocialIcons";
+import SocialIcons from "./SocialIcons"
 
-export default function Layout({meta, children}) {
-    return (
+import social from '../data/social.json'
 
-        <>
-    <Head>
-    <meta charSet="utf-8" />
-    <title>{meta?.title || "Hello"} • Nelson Estevão</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="/img/wave.gif" type="image/gif" />
-    </Head>
+const Layout = ({ meta, children }) => (
+    <>
+        <Head>
+            <meta charSet="utf-8" />
+            <title>{meta?.title || "Hello"} • Nelson Estevão</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="shortcut icon" href="/img/wave.gif" type="image/gif" />
+        </Head>
 
-    <div className="avatar">
-    <Image src="/img/profile.png" alt="Nelson Estevão" width="1500" height="1500" />
-</div>
-    <div className="content">
-        {children}
+        <div className="avatar">
+            <Image src="/img/profile.png" alt="Nelson Estevão" width="1500" height="1500" />
+        </div>
 
-        <SocialIcons />
-    </div></>
-    )
-}
+        <div className="content">
+            {children}
+
+            <SocialIcons twitter={social.twitter} linkedin={social.linkedin} github={social.github} gitlab={social.gitlab} />
+        </div>
+    </>
+)
+
+export default Layout
--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -1,39 +1,12 @@
+import styles from '../styles/SocialIcons.module.css'
 
-export default function SocialIcons() {
-    return (
-      <ul className="social-icons">
-        <li>
-          <a
-            className="twitter"
-            href="https://twitter.com/nelsonmestevao"
-            title="Twitter"
-            >Twitter</a
-          >
-        </li>
-        <li>
-          <a
-            className="linkedin"
-            href="https://linkedin.com/in/nelsonmestevao"
-            title="LinkedIn"
-            >LinkedIn</a
-          >
-        </li>
-        <li>
-          <a
-            className="github"
-            href="https://github.com/nelsonmestevao"
-            title="GitHub"
-            >GitHub</a
-          >
-        </li>
-        <li>
-          <a
-            className="gitlab"
-            href="https://gitlab.com/nelsonmestevao#"
-            title="GitLab"
-            >GitLab</a
-          >
-        </li>
-      </ul>
-    )
-}
+const SocialIcons = ({ twitter, linkedin, github, gitlab }) => (
+  <ul className="social-icons">
+    { twitter && <li><a className={styles.twitter} href={`https://twitter.com/${twitter}`} title="Twitter" >Twitter</a></li>}
+    { linkedin && <li><a className={styles.linkedin} href={`https://linkedin.com/in/${linkedin}`} title="LinkedIn" >LinkedIn</a></li>}
+    { github && <li><a className={styles.github} href={`https://github.com/${github}`} title="GitHub" >GitHub</a></li>}
+    { gitlab && <li><a className={styles.gitlab} href={`https://gitlab.com/${gitlab}`} title="GitLab" >GitLab</a></li>}
+  </ul>
+)
+
+export default SocialIcons
