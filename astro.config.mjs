8b5bce8e2c3328c6edbb7f2aa3ea01adb569a591
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-20 19:10:10 +0100

    Optimize image upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [image(), mdx(), react()]
 });
